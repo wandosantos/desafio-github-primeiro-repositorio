f5a01e0e3cc09a9dcdd53f52b7442f382c1723a9
Wando santos <satevandro@gmail.com>
2021-12-11 16:17:03 -0300

    Aula react Eduzz

---
+++ react/aulareact/src/Corpo.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import ReactDOM from 'react-dom'
+import './index.css'
+
+
+
+
+
+
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+    <Corpo/>,rootElement
+)
